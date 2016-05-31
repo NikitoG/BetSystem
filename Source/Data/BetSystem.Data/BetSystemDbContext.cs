641b3ac0d3bbf4e140c28fdf8fac2808d002c724
NikitoG <niki_spike@yahoo.com>
2016-06-01 00:59:49 +0300

    Extract User and DbContext

---
+++ Source/Data/BetSystem.Data/BetSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace BetSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class BetSyStemDbContext : IdentityDbContext<User>
+    {
+        public BetSyStemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BetSyStemDbContext Create()
+        {
+            return new BetSyStemDbContext();
+        }
+    }
+}
