5c9a6fa936636c44abb7275d01e6d054262f9fee
NikitoG <niki_spike@yahoo.com>
2016-06-02 23:10:10 +0300

    Add migrations.

---
+++ Source/Web/BetSystem.Web.Api/App_Start/DatabaseConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace BetSystem.Web.Api.App_Start
+{
+    using System.Data.Entity;
+
+    using BetSystem.Data;
+    using BetSystem.Data.Migrations;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BetSyStemDbContext, Configuration>());
+        }
+    }
+}
